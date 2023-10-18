d1bd632714597d16b218a08860d187a241640c7c
Vera Samohina <samohinavera44@gmail.com>
2023-10-18 14:38:42 +0300

    add field user for mailinglog

--- mailing/services.py
+++ mailing/services.py
@@ -1,52 +1,49 @@
 from smtplib import SMTPException
-
-from django.conf import settings
 from django.core.mail import send_mail
 from datetime import timedelta, datetime, timezone
 from mailing.models import Mailing, MailingLog
+from django.conf import settings
 
 current_datetime = datetime.now(timezone.utc)
-print(current_datetime)
 
 
 def send_mailings():
     mailings_completed = Mailing.objects.filter(is_active=True, end_time__lte=current_datetime)
     for mailing in mailings_completed:
         mailing.status = 'completed'
-    print(mailings_completed)
     mailing_list = Mailing.objects.filter(is_active=True, status__in=['created', 'started'],
-                                          start_time__lte=current_datetime, end_time__gte=current_datetime)
-    print(mailing_list)
+                                      start_time__lte=current_datetime, end_time__gte=current_datetime)
+    print(f'будут отправлены следующие рассылки {mailing_list}')
     for mailing in mailing_list:
         client_emails = [client.email for client in mailing.clients.all()]
         message = mailing.message
-        print(f'время в начале прохода {mailing.start_time}')
-        try:
-            send_mail(
-                subject=message.title,
-                message=message.body,
-                from_email=settings.EMAIL_HOST_USER,
-                recipient_list=client_emails,
-                fail_silently=False
-            )
-            log = MailingLog.objects.create(status='success', response='Email sent successfully', mailing=mailing)
-            log.save()
-            mailing.start_time = log.timestamp
-            print(f'время после присваивания времени Log {mailing.start_time}')
+        user = mailing.owner
+        for client_email in client_emails:
+            print(client_email)
+            try:
+                send_mail(
+                    subject=message.title,
+                    message=message.body,
+                    from_email=settings.EMAIL_HOST_USER,
+                    recipient_list=[client_email],
+                )
+                log = MailingLog.objects.create(status='success', response='Email sent successfully',
+                                                mailing=mailing, user=user)
+                log.save()
+                mailing.start_time = log.timestamp
+                if mailing.period == 'daily':
+                    mailing.start_time += timedelta(days=1)
+                    mailing.status = 'started'
+                elif mailing.period == 'weekly':
+                    mailing.start_time += timedelta(days=7)
+                    mailing.status = 'started'
+                elif mailing.period == 'monthly':
+                    mailing.start_time += timedelta(days=30)
+                    mailing.status = 'started'
+                elif mailing.period == 'one_time':
+                    mailing.status = 'completed'
+                mailing.save()
 
-            if mailing.period == 'daily':
-                mailing.start_time += timedelta(days=1)
-                mailing.status = 'started'
-            elif mailing.period == 'weekly':
-                mailing.start_time += timedelta(days=7)
-                mailing.status = 'started'
-            elif mailing.period == 'monthly':
-                mailing.start_time += timedelta(days=30)
-                mailing.status = 'started'
-            elif mailing.period == 'one_time':
-                mailing.status = 'completed'
-            mailing.save()
-            print(f' время после mailing(save() {mailing.start_time}')
-        except SMTPException as e:
-            log = MailingLog.objects.create(status='error', response=str(e), mailing=mailing)
-            log.save()
+            except SMTPException as e:
+                log = MailingLog.objects.create(status='error', response=str(e), mailing=mailing, user=user)
+                log.save()
